35bd084fa3487357408fc880d5acc71e5dfdcb53
Peter Heesterman <heesterman.peter@gmail.com>
2018-07-09 22:06:47 +1200

    Make a basic enumerator

---
+++ src/enumerator/inputDefinitions.js
@@ -0,0 +1,30 @@
+
+const inputDefinitions = [
+  {
+    name: 'empty object',
+    literal: '{}',
+    type: Object
+  },
+  {
+    name: 'empty array',
+    literal: '[]',
+    type: Array
+  },
+  {
+    name: 'zero',
+    literal: '0',
+    type: Number
+  },
+  {
+    name: 'positive number',
+    literal: '123',
+    type: Number
+  },
+  {
+    name: 'negative number',
+    literal: '-456',
+    type: Number
+  }
+]
+
+module.exports = inputDefinitions
---
+++ src/utils/getSpecFilePath.spec.js
@@ -0,0 +1,40 @@
+
+test('Anonymous function: (filePath) => {...} should XXX with input (empty object)', () => {
+  expext(Anonymous function: (filePath) => {...}({})).toBe(undefined)
+}
+
+test('Anonymous function: (filePath) => {...} should XXX with input (empty array)', () => {
+  expext(Anonymous function: (filePath) => {...}([])).toBe(undefined)
+}
+
+test('Anonymous function: (filePath) => {...} should XXX with input (zero)', () => {
+  expext(Anonymous function: (filePath) => {...}(0)).toBe(undefined)
+}
+
+test('Anonymous function: (filePath) => {...} should XXX with input (positive number)', () => {
+  expext(Anonymous function: (filePath) => {...}(123)).toBe(undefined)
+}
+
+test('Anonymous function: (filePath) => {...} should XXX with input (negative number)', () => {
+  expext(Anonymous function: (filePath) => {...}(-456)).toBe(undefined)
+}
+
+test('placeholder should XXX with input (empty object)', () => {
+  expext(placeholder({})).toBe(undefined)
+}
+
+test('placeholder should XXX with input (empty array)', () => {
+  expext(placeholder([])).toBe(undefined)
+}
+
+test('placeholder should XXX with input (zero)', () => {
+  expext(placeholder(0)).toBe(undefined)
+}
+
+test('placeholder should XXX with input (positive number)', () => {
+  expext(placeholder(123)).toBe(undefined)
+}
+
+test('placeholder should XXX with input (negative number)', () => {
+  expext(placeholder(-456)).toBe(undefined)
+}
